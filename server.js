89cced9848810eda1e77445445aaa8e6f5a2238f
hdigiorgi <me@hdigiorgi.com>
2019-02-18 14:57:44 -0300

    add helm

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ var cors        = require('cors');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
+const helmet = require('helmet')
 
 var app = express();
+app.use(helmet.frameguard({ action: 'sameorigin' }))
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -17,6 +19,8 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet.dnsPrefetchControl())
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }))
 
 //Sample front-end
 app.route('/b/:board/')
